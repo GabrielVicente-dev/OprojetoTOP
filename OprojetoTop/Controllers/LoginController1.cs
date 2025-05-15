de7d423ba5f7e275a94839e0df604213d5a0cf97
GabrielVicente-Dev <gabz07santos@gmail.com>
2025-05-14 21:02:30 -0300

    modificando Login e Produto Controller

---
+++ OprojetoTop/Controllers/LoginController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OprojetoTop.Controllers
+{
+    public class LoginController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OprojetoTop/Controllers/ProdutoController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OprojetoTop.Controllers
+{
+    public class ProdutoController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
